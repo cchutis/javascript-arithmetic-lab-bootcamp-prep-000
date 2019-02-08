e5c0f9cb24c1f1d00410cd8c8512fc102a4f3d62
Constantine Chutis <constantinechutis@gmail.com>
2019-02-08 19:36:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,19 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b;
 }
+
+function inc(n) {
+  return n++;
+}
+
+function dec(n) {
+  return n--;
+}
+
+function makeInt(n, 10) {
+  return parseInt(n);
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n);
+}
